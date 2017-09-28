642363abbd61c5c2b972e3fa132a787f7c89e274
Svein Olav Risdal <svein.olav.risdal@gmail.com>
2017-09-28 22:11:50 +0200

    Added server

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/diceware-generator.jar clojure.main -m diceware-generator.server
---
+++ src/diceware_generator/server.clj
@@ -0,0 +1,49 @@
+(ns diceware-generator.server
+  (:require [compojure.core :refer [defroutes GET PUT POST DELETE ANY]]
+            [compojure.handler :refer [site]]
+            [compojure.route :as route]
+            [clojure.java.io :as io]
+            [ring.util.response :refer [response content-type]]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.middleware.resource :refer [wrap-resource]]
+            [ring.middleware.content-type :refer [wrap-content-type]]
+            [ring.middleware.not-modified :refer [wrap-not-modified]]
+            [ring.adapter.jetty :refer [run-jetty]]
+            [environ.core :refer [env]]))
+
+(defn splash []
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body "Hello from Heroku"})
+
+(defroutes routes
+  (GET "/" []
+    #_{:status 200
+     :headers {"Content-Type" "text/html"}
+     :body (slurp (io/resource "public/index.html"))}
+     (slurp (io/resource "public/index.html"))
+    #_(splash))
+  (GET "*" [filepath]
+    (slurp (io/resource (str "public/" filepath))))
+  (ANY "*" []
+    (route/not-found (slurp (io/resource "404.html")))))
+
+(defn handler [request]
+  (-> (response "Hello world")
+      (content-type "text/plain")
+      (wrap-resource "public")
+      (wrap-content-type)
+      (wrap-not-modified)))
+
+(def application (wrap-defaults routes site-defaults))
+
+(defn -main [& [port]]
+  (let [port (Integer. (or port (env :port) 5000))]
+    #_(run-jetty (site #'app) {:port port :join? false})
+    (run-jetty #'application
+               {:port port
+                :join? false})))
+
+;; For interactive development:
+;; (.stop server)
+;; (def server (-main))
